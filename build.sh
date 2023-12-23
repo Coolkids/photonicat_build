e2e04a7c6025d565e5defba28d55ab396dcaa398
s196774065 <s196774065@gmail.com>
2023-12-23 11:41:44 +0800

    添加脚本

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+git clone https://github.com/coolsnowwolf/lede.git
+cd lede
+cat << EOF >> feeds.conf.default
+src-git diy1 https://github.com/xiaorouji/openwrt-passwall-packages.git;main
+src-git diy2 https://github.com/xiaorouji/openwrt-passwall.git;main
+src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main
+EOF
+./scripts/feeds update -a
+rm -rf feeds/packages/lang/golang/
+rm -rf feeds/packages/net/tailscale/
+unzip ../configs/golang.zip -d ../configs/
+unzip ../configs/tailscale.zip -d ../configs/
+mv ../configs/golang feeds/packages/lang/
+mv ../configs/tailscale feeds/packages/net/
+./scripts/feeds install -a
+cp ../configs/base_config .config
+make defconfig
+make download -j8 || make download -j1 V=s
+rm -rf $(find ./dl/ -size -1024c)
+make -j$(nproc) || make -j1 V=s
