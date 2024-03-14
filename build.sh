8c9ec7db6a51d848521ddf5f14a090bd416d7f3a
Coolkid <s196774065@gmail.com>
2024-03-14 17:00:11 +0800

    ci  空间问题

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+workdir=`pwd`
 sudo mkdir -p -m 777 /mnt/openwrt
 git clone https://github.com/immortalwrt/immortalwrt.git /mnt/openwrt/immortalwrt
 ln -s /mnt/openwrt/immortalwrt ./immortalwrt
@@ -10,12 +11,11 @@ src-git diy2 https://github.com/xiaorouji/openwrt-passwall.git;main
 src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main
 EOF
 rm target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-photonicat.dts
-cp ../configs/rk3568-photonicat-usb30.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-photonicat.dts
+cp $workdir/configs/rk3568-photonicat-usb30.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-photonicat.dts
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-cp ../configs/base_config .config
+cp $workdir/configs/base_config .config
 make defconfig
 make download -j8 || make download -j1 V=s
 df -h
-rm -rf $(find ./dl/ -size -1024c)
 make -j$(nproc) || make -j1 V=s
