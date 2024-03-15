8687adeb323e6a02fac6f17f46df5afdf3cc9334
Coolkid <s196774065@gmail.com>
2024-03-15 16:51:16 +0800

    update ci

--- tag.sh
+++ tag.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 CURRENT_DATE=$(date +'%y.%m')
+echo "Tag check"
 if git rev-parse -q --verify "refs/tags/v$CURRENT_DATE" >/dev/null; then 
 	echo "Tag 'v$CURRENT_DATE' exists. Deleting..."
 	git tag -d "v$CURRENT_DATE"
