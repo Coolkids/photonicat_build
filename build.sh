9fdb1b578d00178c92af74473ad0a68d81e3a938
coolkid <s196774065@gmail.com>
2024-12-16 09:35:39 +0800

    更新CI

--- build.sh
+++ build.sh
@@ -3,18 +3,18 @@ workdir=`pwd`
 sudo mkdir -p -m 777 /mnt/openwrt
 git clone https://github.com/immortalwrt/immortalwrt.git -b openwrt-24.10 --single-branch --depth 1 /mnt/openwrt/immortalwrt
 ln -s /mnt/openwrt/immortalwrt ./immortalwrt
-cd immortalwrt
+cp $workdir/tool.sh /mnt/openwrt/immortalwrt/tool.sh
+chmod +x /mnt/openwrt/immortalwrt/tool.sh
+cd /mnt/openwrt/immortalwrt
 df -h
 cat << EOF >> feeds.conf.default
 src-git diy1 https://github.com/xiaorouji/openwrt-passwall-packages.git;main
 src-git diy2 https://github.com/xiaorouji/openwrt-passwall.git;main
 src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main
 EOF
-bash ./tool.sh feed
 rm target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-photonicat.dts
 cp $workdir/configs/rk3568-photonicat-usb30.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-photonicat.dts
-./scripts/feeds update -a
-./scripts/feeds install -a
+./tool.sh feed
 cp $workdir/configs/base_config .config
 make defconfig
 make download -j8 || make download -j1 V=s
